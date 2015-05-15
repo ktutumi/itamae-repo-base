e2be9fb063d68f826e0fd998ff900c01d45f812b
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2015-05-15 15:49:27 +0900

    php56 Node Attribute 対応

--- cookbooks/php56/default.rb
+++ cookbooks/php56/default.rb
@@ -3,11 +3,24 @@
 # Recipe:: default
 #
 
+default_vars = {
+    max_execution_time: '30',
+    memory_limit: '128M',
+    error_reporting: 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
+    display_errors: 'Off',
+    post_max_size: '8M',
+    upload_max_filesize: '2M',
+    date_timezone: 'Asia/Tokyo',
+}
+
+node.reverse_merge!(php: default_vars)
+
 package 'php' do
   options '--enablerepo=remi-php56'
 end
 
 template '/etc/php.ini' do
-  notifies :restart, 'service[httpd]'
+  notifies :reload, 'service[httpd]'
+  variables(node[:php])
 end
 
