a5fb03762da3c3f2dd3ccfdb3da88886119cf4c7
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2015-05-15 16:14:45 +0900

    php56 追加パッケージ、ディレクティブ Node Attribute 対応

--- cookbooks/php56/default.rb
+++ cookbooks/php56/default.rb
@@ -3,24 +3,31 @@
 # Recipe:: default
 #
 
+packages = %w(php php-cli php-devel php-mbstring php-pear)
+
 default_vars = {
-    max_execution_time: '30',
-    memory_limit: '128M',
-    error_reporting: 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
-    display_errors: 'Off',
-    post_max_size: '8M',
-    upload_max_filesize: '2M',
-    date_timezone: 'Asia/Tokyo',
+    additional_packages: [],
+    directives: {
+        max_execution_time: '30',
+        memory_limit: '128M',
+        error_reporting: 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
+        display_errors: 'Off',
+        post_max_size: '8M',
+        upload_max_filesize: '2M',
+        date_timezone: 'Asia/Tokyo',
+    }
 }
 
 node.reverse_merge!(php: default_vars)
 
-package 'php' do
-  options '--enablerepo=remi-php56'
+(packages + node[:php][:additional_packages]).each do |pkg|
+  package pkg do
+    options '--enablerepo=remi-php56'
+  end
 end
 
 template '/etc/php.ini' do
   notifies :reload, 'service[httpd]'
-  variables(node[:php])
+  variables(node[:php][:directives])
 end
 
