ac86b176b9d1bbbe304fdc8dad31c2d97e45071a
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2015-06-26 11:09:35 +0900

    remiのバージョンを変数化

--- cookbooks/remi/default.rb
+++ cookbooks/remi/default.rb
@@ -3,9 +3,11 @@
 # Recipe:: default
 #
 
+version = node[:platform_version][0]
+
 package 'epel-release'
 
-package 'http://rpms.famillecollet.com/enterprise/remi-release-6.rpm' do
+package "http://rpms.famillecollet.com/enterprise/remi-release-#{version}.rpm" do
   not_if 'rpm -q remi-release'
 end
 
